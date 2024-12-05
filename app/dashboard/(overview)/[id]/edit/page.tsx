a1d4acdbcdb1cd3aa39b8ce175e073798fa5b80b
mkApproach <kinai-ap@cameo.plala.or.jp>
2024-12-05 12:20:56 +0900

    firstCommit

--- app/dashboard/(overview)/[id]/edit/page.tsx
+++ app/dashboard/(overview)/[id]/edit/page.tsx
@@ -3,7 +3,7 @@ import Breadcrumbs from '@/app/ui/receptions/breadcrumbs';
 import { fetchReceptionById, fetchClubs, fetchCategorys } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
  
-export default async function Page(props: { params: Promise<{ id: string }> }) {
+export default async function Page(props: { params: Promise<{ id: number }> }) {
     const params = await props.params;
     const id = params.id;
     const [reception, clubs, categorys] = await Promise.all([
