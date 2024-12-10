161519a29d35a18f49184e44ff9227e27ed55e1f
mkApproach <kinai-ap@cameo.plala.or.jp>
2024-12-10 15:46:48 +0900

    firstCommit

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -35,7 +35,7 @@ export default async function Page(
     return (
         <div className="w-full">
           <div className="flex w-full items-center justify-between">
-            <h1 className={`${lusitana.className} text-2xl`}>受付一覧</h1>
+            <h1 className={`${lusitana.className} text-2xl`}>受付　一覧</h1>
           </div>
           <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
             <Search placeholder="受付の検索..." />
